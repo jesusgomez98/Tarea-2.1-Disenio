246f7e9f2dad545abb64eba7ab127f75a2c0cb07
chungo1998prime <jagomezc@unah.hn>
2025-06-22 15:51:46 -0600

    Funciones para leer y guardar producto

--- index.js
+++ index.js
@@ -2,6 +2,18 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+
+
+
+const getProductos = () => {
+  const data = fs.readFileSync('./productos.json', 'utf8');
+  return JSON.parse(data);
+};
+
+const saveProductos = (productos) => {
+  fs.writeFileSync('./productos.json', JSON.stringify(productos, null, 2));
+};
+
 const PORT = 3000;
 
 app.use(express.json());
