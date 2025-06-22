d1bdd97a46d6ac4a39de4934f14109274b8f38d5
chungo1998prime <jagomezc@unah.hn>
2025-06-22 14:47:52 -0600

    Servidor

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-s
+
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
