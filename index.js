4ed01d1e3f08ec82f34c57c9b49a77708e57c9b3
chungo1998prime <jagomezc@unah.hn>
2025-06-22 17:37:19 -0600

    PUT y DELETE

--- index.js
+++ index.js
@@ -30,7 +30,10 @@ app.get('/productos/:id', (req, res) => {
     return res.status(404).json({ error: 'Producto no encontrado' });
   }
   res.json(producto);
+  
 });
+
+
 
 app.post('/productos', (req, res) => {
   const { nombre, precio, descripcion, disponible } = req.body;
@@ -39,10 +42,10 @@ app.post('/productos', (req, res) => {
     return res.status(400).json({ error: 'El nombre es obligatorio' });
   }
   if (typeof precio !== 'number' || precio <= 0) {
-    return res.status(400).json({ error: 'El precio debe ser un número positivo' });
+    return res.status(400).json({ error: 'El precio debe ser un numero positivo' });
   }
   if (!descripcion || descripcion.length < 10) {
-    return res.status(400).json({ error: 'La descripción debe tener al menos 10 caracteres' });
+    return res.status(400).json({ error: 'La descripcion debe tener al menos 10 caracteres' });
   }
 
   const productos = getProductos();
@@ -63,6 +66,62 @@ app.post('/productos', (req, res) => {
 
 
 
+app.put('/productos/:id', (req, res) => {
+  const productos = getProductos();
+  const productoIndex = productos.findIndex(p => p.id === parseInt(req.params.id));
+
+  if (productoIndex === -1) {
+    return res.status(404).json({ error: 'Producto no encontrado' });
+  }
+
+  const { nombre, precio, descripcion, disponible } = req.body;
+
+  if (nombre !== undefined) productos[productoIndex].nombre = nombre;
+  if (precio !== undefined) {
+    if (typeof precio !== 'number' || precio <= 0) {
+      return res.status(400).json({ error: 'El precio debe ser un número positivo' });
+    }
+    productos[productoIndex].precio = precio;
+  }
+  if (descripcion !== undefined) {
+    if (descripcion.length < 10) {
+      return res.status(400).json({ error: 'La descripción debe tener al menos 10 caracteres' });
+    }
+    productos[productoIndex].descripcion = descripcion;
+  }
+  if (disponible !== undefined) productos[productoIndex].disponible = disponible;
+
+  saveProductos(productos);
+  res.json(productos[productoIndex]);
+});
+
+
+app.delete('/productos/:id', (req, res) => {
+  let productos = getProductos();
+  const productoIndex = productos.findIndex(p => p.id === parseInt(req.params.id));
+
+  if (productoIndex === -1) {
+    return res.status(404).json({ error: 'Producto no encontrado' });
+  }
+
+  const productoEliminado = productos.splice(productoIndex, 1);
+  saveProductos(productos);
+
+  res.json({ mensaje: 'Producto eliminado', producto: productoEliminado[0] });
+});
+
+
+app.get('/productos/disponibles', (req, res) => {
+  const productos = getProductos();
+  const disponibles = productos.filter(p => p.disponible === true);
+  res.json(disponibles);
+});
+
+
+
+
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
+
+console.log("Servidor inciado")
