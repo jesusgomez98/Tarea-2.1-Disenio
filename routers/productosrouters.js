dcc79308db2b5f749dc799a4a589edff1cf78827
chungo1998prime <jagomezc@unah.hn>
2025-07-15 23:09:55 -0600

    Routers

---
+++ routers/productosrouters.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { obtenerProductos } from '../controllers/productosController.js';
+
+const router = express.Router();
+
+router.get('/', obtenerProductos);
+
+export default router;
